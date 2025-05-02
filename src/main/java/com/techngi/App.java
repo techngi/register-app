cc7b44dce2dff9569b062d0e09f07732b6a5fb61
techngi <sanaqvi573@gmail.com>
2025-05-02 21:22:55 +0000

    Keep app running to prevent pod crash

--- src/main/java/com/techngi/App.java
+++ src/main/java/com/techngi/App.java
@@ -4,10 +4,10 @@ package com.techngi;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello World!");
+        Thread.sleep(Long.MAX_VALUE); // Keeps app running forever
     }
 }
+
